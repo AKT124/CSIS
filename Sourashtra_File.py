7e1d8b56f976b4b99b6917316096fc79d4bbb32b
AKT124 <k1340934@students.katyisd.org>
2026-09-10 09:51:30 -0500

    Add files via upload
    - Formatted the sourashtra file
    -Created Stat calculation file
    - Created placeholders for some of the GUI components

---
+++ Sourashtra_File.py
@@ -0,0 +1,181 @@
+name = "Sourashtra"
+words = """"poɭɭo fruit 
+moʈʈɑppɑn leadership 
+beʈki female 
+ɑmbɑʈ sour 
+phɑrɑdu after 
+ɑphis office 
+bhoɳɳo pot 
+lubhim gooseberry pickles 
+tinvo third 
+nɑdini grand daughter 
+devɑr husband's younger brother 
+dɑdo elder brother 
+thɑm place 
+sthiti status 
+drɑccɑ poɭɭo grapes 
+cɑʈʈugeri suddenly 
+beɖo son 
+ɖɑppo drum 
+kɑɖɑle ground nut 
+cuɭɭo oven 
+bencu bench 
+dʒendʒɑm holy thread 
+bɑdʒdʒi greens 
+chelmo river spring 
+dʒhɑnni broom stick 
+kɑvɑɖ door 
+cokkɑɪ shirit (illegible) 
+gosulo anklit 
+ɑngili finger 
+khɑɖo take 
+vinkhɑr weaver 
+ghɑɖi throat 
+likhɑɖo make to write 
+piɭɭo child 
+pɑjmpoʈ worship 
+biːt wall 
+ɑmbɑʈ sour 
+phille to wear 
+solophɑr morning 
+bhouŋɖi daughter-in-law 
+lubhin gooseberry pickles 
+tɑndu rice 
+nɑdini/nɑtini grand daughter 
+dekki to meet 
+dɑdo elder brother 
+No transcription given No gloss given 
+stidi status 
+dhɑɖɖe send 
+cɑʈukeri suddenly 
+beɖo sun 
+ɖuɳɖum drum 
+kɑɖɑle teːl ground nut oil 
+cumbi pinch 
+benc bench 
+dʒendʒɑm holy thread 
+kudʒal white 
+chenno parched Benhal grain seasoned with tumeric salt 
+dʒɑgɑtte concel (illegible) 
+kɑccɑmbɑ poɭɭo mango 
+gɑɭɖe scold 
+No transcription given No gloss given 
+vinkher weaver 
+No transcription given No gloss given 
+No transcription given No gloss given 
+likho make to write 
+peʈi box 
+bɑːjmpoʈ worship 
+bijɑt key 
+ɑmbɑʈ sour 
+bhuk hungry 
+solobhar morning 
+bhɑt teʈʈar plate 
+ɑbbɑtte obtain 
+teʈʈi knock 
+nɑtini grand daughter 
+dobbi push 
+pɑndɑre fifteen 
+the ludiro oil lamp 
+mɑdhip status, suspect 
+dhegimbɑt curd rice 
+ceʈukeri suddenly 
+dhuvɑtte worship 
+ɖuɳɖɑm drum 
+dekkɑɖɑtte to show 
+sigir sneeze 
+bench bench 
+dʒenkɑɭɑm blanket 
+mɑndʒiri cat 
+chenno parched Bengal grain saesoned with tumeric salt 
+dʒɑnni broom stick 
+ghɑɖi throat 
+No transcription given No gloss given 
+guɭɭejar Jaggery 
+guɖɖu pongɑl sugar rice 
+khoɭti bag 
+vinkher weaver 
+ghoɖo horse 
+pɑɖi yarn 
+pɑːjmpoːʈ worship 
+bɑɭɑːr wantonly 
+kobbo when 
+phonni comb 
+ɑːfis office 
+No transcription given singer 
+lubhim gooseberry pickles 
+tinviːs sixty 
+nɑttini grand daughter 
+divɑjo second 
+dɑdo elder brother 
+thɑndu rice 
+sthirɑm stable 
+dhɑɖɖe send 
+udhɑr loan 
+boʈʈu mangalasutra 
+tuɳɖu towel 
+cumʈʈi Transcription illegible 
+benc bench 
+dʒomɑɪ son-in-law 
+bodʒdʒɑl jealousy 
+chennɑ parched Bengal grain saesoned with tumeric salt 
+dʒɑɖɑni husband's elder brother's wife 
+kɑvɑʈ door 
+cekki to cut 
+guɭɭe sweeter 
+guɭɭu Jaggery 
+khuɭʈi bag 
+vinkhɑr weaver 
+poɭi spoon 
+kɑːppi coffee 
+boɳɖi cart 
+firi turn 
+bhonna pot 
+kobhem ever 
+teʈʈi knock 
+nɑtini grand daughter 
+dori thread 
+tɑndu rice 
+dhuvɑtti spoon for fragrant smoke of frankincense of an offering 
+utttrɑm girder 
+kuɖo cock 
+kɑdʒdʒu sore, ulcer 
+couɭi jacket 
+dʒeɖbulo head ornament 
+bɑdʒdʒi greens 
+chellɑɖɑm underwear 
+dʒeɖɑni brother 
+kɑɭɭo bittergoard 
+vikku poison 
+khɑndur shoulder 
+vinkhər weaver 
+guddi blow 
+çɑngi to say 
+khoulo niche 
+piliʈi toy 
+tɑppɑʈ boil 
+boʈʈo cloth 
+hibbɑʈ stop 
+phonis poɭɭo jackfruit 
+bhouɳɖi daughter-in-law 
+dobbi push 
+tukki carry, lift 
+nɑtte grand son 
+dɑllo husband 
+dɑdo elder brother 
+thɛki obstruct 
+dhumpɑn tobbaco 
+ɑʈʈɑːleː shelf 
+kuɖi hen 
+cokkɑɪ shirt 
+vincu scorpion 
+dʒelleʈ sift, sieve 
+kɑdʒɑl eyetex
+chekkur wheel 
+dʒɑgo close 
+koɳɖo bran 
+nekku nail 
+goɳɑm skirt 
+vɑngi Eggplant 
+khɑlle take 
+ghelo neck"""""
